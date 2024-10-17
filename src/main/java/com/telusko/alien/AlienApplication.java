4368aadeef4d4ad58082d75c0390403afdd604d7
nayeem6836 <nayeemmaseed9100@gmail.com>
2024-10-17 22:22:20 +0530

    depndency injection

---
+++ src/main/java/com/telusko/alien/AlienApplication.java
@@ -0,0 +1,15 @@
+package com.telusko.alien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlienApplication.class, args);
+		Dev obj = new Dev();
+		obj.run();																																																																																																																																																																																																																																																																																																																																		
+	}
+
+}
---
+++ src/main/java/com/telusko/alien/Dev.java
@@ -0,0 +1,7 @@
+package com.telusko.alien;
+
+public class Dev{
+    public void run(){
+        System.out.println("welcome");
+    }
+}
